702916a682312e18ef26cc84b1d41f8f878104f8
yegor_shalaiev <yegorshalaev@gmail.com>
2024-05-04 14:19:00 +0300

    MTX-4 EDIT: eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+	],
+	ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+	rules: {
+		'max-len': ['error', { 'code': 120 }],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		indent: ['error', 'tab'],
+		'comma-dangle': ['error', 'always-multiline'],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
